c22760a59ef3a49e2d2fb7fe729d450faea033d5
ANDRESCO2022 <98791167+ANDRESCO2022@users.noreply.github.com>
2022-09-18 21:09:37 -0500

    update dist

--- todoApp/src/Components/ErrorComponent.jsx
+++ todoApp/src/Components/ErrorComponent.jsx
@@ -1,4 +1,4 @@
-export const ErrorComponent = ({message}) => {
+export const ErrorComponent = ({ message }) => {
 	return (
 		<div className="p-3 mb-3 rounded md bg-red-600	 font-bold-400 upercase text-center text-white">
 			<p>{message}</p>
